e5ac05ce51458366267bef9d8f5d34ed8df314a9
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-03-14 14:40:29 +0000

    display the answer at View

--- Eq2Grau/Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -38,8 +38,7 @@ namespace Eq2Grau.Controllers {
          // 1.
          if (string.IsNullOrWhiteSpace(A) || string.IsNullOrWhiteSpace(B) || string.IsNullOrWhiteSpace(C)) {
             // enviar mensagem para o utilizador
-
-
+            ViewBag.Mensagem = "The A, B and C parameters can not be empty.";
 
             // devolver controlo à View
             return View();
@@ -49,8 +48,7 @@ namespace Eq2Grau.Controllers {
          if (!double.TryParse(A, out auxA)) {
             // o A não é número.
             // enviar mensagem para o utilizador
-
-
+            ViewBag.Mensagem = "The parameter A must be a number.";
 
             // devolver controlo à View
             return View();
@@ -60,8 +58,7 @@ namespace Eq2Grau.Controllers {
          if (!double.TryParse(B, out auxB)) {
             // o B não é número.
             // enviar mensagem para o utilizador
-
-
+            ViewBag.Mensagem = "The parameter B must be a number.";
 
             // devolver controlo à View
             return View();
@@ -71,7 +68,7 @@ namespace Eq2Grau.Controllers {
          if (!double.TryParse(C, out auxC)) {
             // o C não é número.
             // enviar mensagem para o utilizador
-
+            ViewBag.Mensagem = "The parameter C must be a number.";
 
             // devolver controlo à View
             return View();
@@ -82,7 +79,7 @@ namespace Eq2Grau.Controllers {
          if (auxA == 0) {
             // o A é ZERO.
             // enviar mensagem para o utilizador
-
+            ViewBag.Mensagem = "The parameter A can not be 0 (zero).";
 
             // devolver controlo à View
             return View();
@@ -96,8 +93,9 @@ namespace Eq2Grau.Controllers {
             string x1 = Math.Round((-auxB + Math.Sqrt(delta)) / 2 / auxA, 2) + "";
             string x2 = Math.Round((-auxB - Math.Sqrt(delta)) / 2 / auxA, 2) + "";
             // enviar mensagem para o utilizador
-
-
+            ViewBag.Mensagem = "There are two real diferent roots";
+            ViewBag.X1 = x1;
+            ViewBag.X2 = x2;
 
             // devolver controlo à View
             return View();
@@ -106,7 +104,9 @@ namespace Eq2Grau.Controllers {
          if (delta == 0) {
             string x = Math.Round((-auxB + Math.Sqrt(delta)) / 2 / auxA, 2) + "";
             // enviar mensagem para o utilizador
-
+            ViewBag.Mensagem = "There are two real roots, but equals";
+            ViewBag.X1 = x;
+            ViewBag.X2 = x;
 
             // devolver controlo à View
             return View();
@@ -116,7 +116,9 @@ namespace Eq2Grau.Controllers {
             string x1 = Math.Round((-auxB) / 2 / auxA, 2) + " + " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
             string x2 = Math.Round((-auxB) / 2 / auxA, 2) + " - " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
             // enviar mensagem para o utilizador
-
+            ViewBag.Mensagem = "There are two complex roots";
+            ViewBag.X1 = x1;
+            ViewBag.X2 = x2;
 
             // devolver controlo à View
             return View();
